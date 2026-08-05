34816a294789044c7434c4af0de239ed519e3dea
moontai0724 <moontai0724@gmail.com>
2026-08-06 01:33:59 +0800

    feat: sort rooms

--- src/data-manager/daily.ts
+++ src/data-manager/daily.ts
@@ -18,9 +18,20 @@ export class DailySessionManager {
     if (!this.endsAt || session.endsAt > this.endsAt)
       this.endsAt = session.endsAt;
 
-    if (session.room) this.activeRooms.set(session.room.id, session.room);
+    if (session.room) {
+      this.activeRooms.set(session.room.id, session.room);
+      this.orderActiveRooms();
+    }
     if (session.type) this.activeTypes.set(session.type.id, session.type);
     for (const speaker of session.speakers)
       this.activeSpeakers.set(speaker.id, speaker);
+  }
+
+  private orderActiveRooms(): void {
+    const activeRooms = Array.from(this.activeRooms.entries()).sort();
+    this.activeRooms.clear();
+    for (const [roomId, room] of activeRooms) {
+      this.activeRooms.set(roomId, room);
+    }
   }
 }

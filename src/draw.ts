d31ccf8437c206ec9c39f655e8b25da2319c0fc3
moontai0724 <moontai0724@gmail.com>
2026-08-06 00:59:39 +0800

    feat: common function to draw border

---
+++ src/draw.ts
@@ -0,0 +1,17 @@
+export function drawSolidBorder(
+  range: GoogleAppsScript.Spreadsheet.Range,
+  color = "black",
+) {
+  return range
+    .setBorder(
+      true,
+      true,
+      true,
+      true,
+      false,
+      false,
+      color,
+      SpreadsheetApp.BorderStyle.SOLID_MEDIUM,
+    )
+    .activate();
+}

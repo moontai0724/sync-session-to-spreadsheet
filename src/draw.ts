5097db254033c4a39ab29a1c9e54d6a16cd3c066
moontai0724 <moontai0724@gmail.com>
2026-08-06 12:58:29 +0800

    fix: remove force focus

--- src/draw.ts
+++ src/draw.ts
@@ -2,16 +2,14 @@ export function drawSolidBorder(
   range: GoogleAppsScript.Spreadsheet.Range,
   color = "black",
 ) {
-  return range
-    .setBorder(
-      true,
-      true,
-      true,
-      true,
-      false,
-      false,
-      color,
-      SpreadsheetApp.BorderStyle.SOLID_MEDIUM,
-    )
-    .activate();
+  return range.setBorder(
+    true,
+    true,
+    true,
+    true,
+    false,
+    false,
+    color,
+    SpreadsheetApp.BorderStyle.SOLID_MEDIUM,
+  );
 }
